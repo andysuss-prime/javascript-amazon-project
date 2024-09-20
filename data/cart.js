a82d360b59aafb6d25ccfa07658789f45e62b772
Andy Suss <andy@primewineandspirits.com>
2024-09-20 07:36:06 -0400

    Save cart to localstorage

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,8 @@
-export let cart = [
-  {
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2,
-  },
-  {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1,
-  },
-];
+export let cart = JSON.parse(localStorage.getItem("cart"));
+if (!cart) {
+  cart = [];
+}
+
 export function removeFromCart(productId) {
   const newCart = [];
 
@@ -18,6 +13,11 @@ export function removeFromCart(productId) {
   });
 
   cart = newCart;
+  saveToStorage();
+}
+
+function saveToStorage() {
+  localStorage.setItem("cart", JSON.stringify(cart));
 }
 
 export function addToCart(productId) {
@@ -41,4 +41,5 @@ export function addToCart(productId) {
       quantity: quantity,
     });
   }
+  saveToStorage();
 }
