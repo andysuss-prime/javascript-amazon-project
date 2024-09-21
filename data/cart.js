ed0ab535a8294ecc95a01cd7c4a0507d098c27b6
Andy Suss <andy@primewineandspirits.com>
2024-09-21 10:45:58 -0400

    orderSummary Isolation

--- data/cart.js
+++ data/cart.js
@@ -1,6 +1,17 @@
 export let cart = JSON.parse(localStorage.getItem("cart"));
 if (!cart) {
-  cart = [];
+  cart = [
+    {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 2,
+      deliveryOptionId: "1",
+    },
+    {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 1,
+      deliveryOptionId: "2",
+    },
+  ];
 }
 
 export function removeFromCart(productId) {
@@ -39,11 +50,11 @@ export function addToCart(productId) {
     cart.push({
       productId: productId,
       quantity: quantity,
+      deliveryOptionId: "1",
     });
   }
   saveToStorage();
 }
-
 
 export function calculateCartQuantity() {
   let cartQuantity = 0;
@@ -53,7 +64,7 @@ export function calculateCartQuantity() {
   });
 
   return cartQuantity;
-};
+}
 
 export function updateQuantity(productId, newQuantity) {
   let matchingItem;
@@ -65,6 +76,20 @@ export function updateQuantity(productId, newQuantity) {
   });
 
   matchingItem.quantity = newQuantity;
+
+  saveToStorage();
+}
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  matchingItem.deliveryOptionId = deliveryOptionId;
 
   saveToStorage();
 }
--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -1,9 +1,16 @@
-deliveryOptions: [
-
-{id:'1', name: "Standard Delivery", price: 0, days: 7},
-
-{id:'2', name: "Express Delivery", price: 500, days: 3},
-
-{id:'3', name: "Next Day Delivery", price: 1000, days: 1}
-
+export const deliveryOptions = [
+  { id: "1", name: "Standard Delivery", priceCents: 0, deliveryDays: 7 },
+  { id: "2", name: "Express Delivery", priceCents: 499, deliveryDays: 3 },
+  { id: "3", name: "Next Day Delivery", priceCents: 999, deliveryDays: 1 },
 ];
+export function getDeliveryOption(deliveryOptionId) {
+  let deliveryOption;
+
+  deliveryOptions.forEach((option) => {
+    if (option.id === deliveryOptionId) {
+      deliveryOption = option;
+    }
+  });
+
+  return deliveryOption || deliveryOptions[0];
+}
